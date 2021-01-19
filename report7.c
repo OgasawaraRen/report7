baa360b797c85551688ee2222dbf6618448d2ee3
Ren Ogasawara <e205701@ie.u-ryukyu.ac.jp>
2021-01-19 19:07:51 +0900

    配列を出力する関数作成

--- report7.c
+++ report7.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
+void printScores(int[], int, char[]);
+
 int main()
 {
     const int SIZE = 5; //定数として配列のサイズを宣言
     int scores[SIZE] = {0, 60, 70, 100, 90};
     //処理コード
+    printScores(scores, SIZE, "scores");
+    //printScores(scores, SIZE, "results");
     return 0;
+}
+
+void printScores(int scores[], int size, char name[])
+{
+    int i;
+
+    printf("%s =", name);
+    for (i = 0; i < size; i++)
+    {
+        printf(" %d", scores[i]);
+    }
+    printf("\n");
 }
