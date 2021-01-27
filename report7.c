c4f43d8723356f22f0f17456e2d12104b9372faf
Ren Ogasawara <e205701@ie.u-ryukyu.ac.jp>
2021-01-27 16:24:51 +0900

    ボゴソート

--- report7.c
+++ report7.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void printScores(int[], int, char[]);
+void bogoSort(int *, int);
+void shuffle(int *, int);
+int orderCheck(int *, int);
 
 int main()
 {
     const int SIZE = 5; //定数として配列のサイズを宣言
     int scores[SIZE] = {0, 60, 70, 100, 90};
     //処理コード
+    srand((unsigned int)time(NULL));
     printScores(scores, SIZE, "scores");
-    //printScores(scores, SIZE, "results");
+    bogoSort(scores, SIZE);
+    printScores(scores, SIZE, "results");
     return 0;
 }
 
@@ -22,4 +29,38 @@ void printScores(int scores[], int size, char name[])
         printf(" %d", scores[i]);
     }
     printf("\n");
+}
+
+void shuffle(int *array, int size)
+{
+    int i;
+    for (i = size - 1; i > 0; i--)
+    {
+        int j = rand() % (i + 1);
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+}
+
+int orderCheck(int *array, int size)
+{
+    int i;
+    for (i = 0; i < size - 1; i++)
+    {
+        if (!(array[i] >= array[i + 1]))
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+void bogoSort(int *array, int size)
+{
+    while (!orderCheck(array, size))
+    {
+        shuffle(array, size);
+    }
 }
